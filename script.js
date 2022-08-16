afd8861631843649829781becbbbbeb1adcf246a
hafsa <hfsyln93@gmail.com>
2022-08-16 17:04:37 +0300

    js ödev 1

---
+++ script.js
@@ -0,0 +1,23 @@
+// ÖDEV1 -HESAP MAKİNASI-
+
+let numb1= +prompt(`enter your first number`)
+console.log(numb1)
+let numb2 = +prompt (`enter your second number`)
+console.log(numb2)
+let oprt = prompt(`enter your operator`)
+console.log(oprt)
+let result =0
+
+    if (oprt == `+`)
+        {result = (numb1 + numb2)}
+    else if (oprt == `-`)
+        {result = (numb1 - numb2)}
+    else if (oprt == `/`)
+        {result = (numb1 / numb2)}
+    else if (oprt == `*`)
+        {result = (numb1 * numb2)};
+    
+        console.log(result)
+
+
+// ödev2 -
