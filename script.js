368b453569bff6f4db6d8677ca166a000a57f2b1
hafsa <hfsyln93@gmail.com>
2022-09-01 15:36:56 +0300

    new

--- script.js
+++ script.js
@@ -869,12 +869,13 @@ console.log(`Ad Soyad birleşimi : `,adSoyad);
 const birlestir = (arr1, arr2) => {
     let adSoyad = [];
     for (let i in arr1) {
-      adSoyad[i] = arr1[i] + arr2[i];
+      adSoyad[i] = arr1[i] + " " + arr2[i];
     }
-console.log(`Ad Soyad birleşim fonksiyon ile : `, adSoyad);
+    
 }
+birlestir(adlar, soyAdlar);
+console.log(`Ad Soyad birleşim fonksiyon ile : `, adSoyad);*/
 
-birlestir(adlar, soyAdlar);*/
 
 
 /***********************ÖRNEK4*** */
@@ -887,7 +888,7 @@ birlestir(adlar, soyAdlar);*/
 //* fonksiyon "ogrenci bulunamadi" dondurulmelidir.
 //*--------------------------------------------------------
 
-const students = [
+/*const students = [
   "ahmet",
   "mehmet",
   "ismet",
@@ -925,3 +926,138 @@ for (let i in arr){
 }
 
 console.log(howMany("ahmet",students))  */
+
+
+/********************************************************* ÖRNEK SORU ÇÖZÜMÜ ******************************/
+
+//! Q-1
+//? Ask the user to input a number. Check the input of the user, if it is not a valid number
+//? then keep getting input from the user until provides a valid number.
+
+ /*const validNumber = () => {
+    let num = prompt("Enter a valid number")
+
+    if (Number(num)){
+         console.log("It is a valid number") 
+   } else {
+        console.log("It is not a valid number") 
+        return validNumber()              // recursion
+     }
+ }
+ console.log(validNumber());*/
+
+
+
+//! Q-2
+//? Write a JavaScript program to compute the greatest common divisor (GCD) (OBEB) of two positive integers.
+
+
+
+/*const ebobNum = (num1, num2) => {
+  let bigNumber;
+  if(num1==0 || num2==0){
+    return `Hatalı giriş yaptınız`
+  }
+
+  for (let i=1; i <= num1 && i <=num2; i++){
+    
+      if (num1%i === 0 && num2%i==0)
+        bigNumber = i
+    }  
+  console.log(bigNumber) // return bigNumber
+}  
+console.log(ebobNum(55, 88));*/
+
+
+/*let num1 = 20
+let num2 = 50
+const ebobNum = () => {
+  let bigNumber;
+  if(num1==0 || num2==0){
+    return `Hatalı giriş yaptınız`
+  }
+
+  for (let i=1; i <= num1; i++){
+    for(let i=1; i <= num2; i++){
+      if (num1%i === 0 && num2%i==0)
+        bigNumber = i
+    }  
+  console.log(bigNumber) // return bigNumber
+}  
+}
+console.log(ebobNum());
+//return ebobNum()  olmuyor*/
+
+
+      //! Q-3
+//? Write a code that displays the factors (tam bolen) of a number entered by the user.
+
+/*let list = []
+const tamBölen = () => {
+let num = +prompt("pozitif sayı giriniz")
+  for (let i = 1; i<=num; i++){
+    if (num%i ===0){
+    list.push(i)
+    } else {  
+    }
+  } 
+  return  list //console.log(list) 
+}
+console.log(tamBölen())*/
+
+//! Q-4
+//? Write a code to calculate the factorial of a number entered by the user.
+
+/*let a =1
+const factl = (n) => {
+  for(let i = 1; i<=n; i++){
+    a = a*i
+  } return a
+}
+console.log(factl(4));*/
+
+//! Q-5
+//? Write a code to find numbers that are divisible by 3 and 5, between 100 and 1000.
+
+
+//! Q-6
+//? Write a code that prints a multiplication table of numbers 1 to 10.
+
+
+//! Q-7
+//? Write a code to draw Whit A
+ 
+//? 5 Whiteball numbers are chosen from 01 to 69 and 1 Powerball number is chosen from 1 to 26.
+
+
+
+//2. Bölüm Functions
+
+//! Q-1
+//? Write a Function that takes 2 parameters (amount and rate) to convert currency.
+
+//! Q-2
+//? Write a JavaScript function that accepts an argument and returns the type.
+
+//! Q-3
+//? You are given a string and character. Write a function to check the string and find how many
+//? times this character occurs in this string.
+//? Example:
+//? str = "Clarusway"         char = "a"       ===> result = 2 as "a" occurs 2 times in Clarusway
+
+//! Q-4
+//? Write a JS code to display Perfect Numbers from 1 to 1000.
+//? A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+//? For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
+
+
+//! Q-5
+//? Develop a guessing game. Randomly generate a number between 0-100. Ask the user to guess that number.
+//? The user should have 5 chance to guess the number.
+//? If the number entered by the user:
+//? is greater than the random number then warn user to decrease,
+//? is less than the random number then warn user to increase,
+//? is equal to the random number then congratulate the user and tell in how many guesses did the user find the number
+//? If user can't find the number in 5 guess then game is lost and display the random number.
+//? Hint: you can use this code to generate random number 0-100
+//? const winingNumber = Math.round(Math.random() * 100);*/
